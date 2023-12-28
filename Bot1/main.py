de1a83965fe74dfcd14a69afdc110e96f9735929
J-Atulya <atulyajayakrishnan@gmail.com>
2023-12-28 13:16:01 +0530

    API added and quote is received

--- Bot1/main.py
+++ Bot1/main.py
@@ -1,5 +1,7 @@
 import discord
 import os
+import requests
+import json
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -8,6 +10,14 @@ client = discord.Client(intents=intents)
 async def on_ready():  #called when the bot is ready to be used
   print('We have logged in as {0.user}'.format(client))
 
+#get quotes from zenquotes.io
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  #the url is taken from the documentation of zenquotes.io
+  json_data = json.loads(response.text) #where did .text come from
+  #a json object contains a datalist with dictionaries with 'q': quote & 'a': author
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
 
 @client.event
 async def on_message(msg):
@@ -18,5 +28,10 @@ async def on_message(msg):
   if msg.content.startswith('$hello'):  #if the msg starts with $hello -> a cmd to bot
     await msg.channel.send('Hello!')  #send Hello! to the channel
 
+  if msg.content.startswith('$inspire'):
+    quote = get_quote()
+    await msg.channel.send(quote)
+
 #running the bot
 client.run(os.getenv('BOT1_TOKEN'))
+
