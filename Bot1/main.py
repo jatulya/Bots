0829a58c42281e29303e681efbc72aa9ee648bab
J-Atulya <atulyajayakrishnan@gmail.com>
2023-12-29 16:33:17 +0530

    Responding and listing

--- Bot1/main.py
+++ Bot1/main.py
@@ -8,16 +8,16 @@ from replit import db
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
+#for user to set if bot should respond to sad words or not
+if "responding" not in db.keys():
+  db["responding"] = True
+
 #list of sad words bot will be checking
 sad_words = ['depressed', 'miserbale', 'sad', 'unhappy', 'angry', 'depressing', 'depression', 'gloomy', 'disheartened', 'discouraged']
 
 #bot will be starting with these, user could add more to the list
 start_enc = ['Cheer up!', 'Hang in there!', 'You are a great person!']
 
-@client.event
-async def on_ready():  #called when the bot is ready to be used
-  print('We have logged in as {0.user}'.format(client))
-  
 #get quotes from zenquotes.io
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
@@ -45,9 +45,14 @@ def del_enc(index):
   if len(enc) > index:
     del enc[index]
     db["encouragement"] = enc
+
+@client.event
+async def on_ready():  #called when the bot is ready to be used
+  print('We have logged in as {0.user}'.format(client))
   
 @client.event
 async def on_message(msg):
+  
   #triggers each time a msg is received
   if msg.author == client.user:  #if the msg is from the bot itself, ignore
     return
@@ -61,15 +66,16 @@ async def on_message(msg):
     quote = get_quote()
     await msg.channel.send(quote)
 
-  #when the bot chooses the enc quote to display, we also need choice of quotes to be from the user submitted quotes
-  options = start_enc
-  if "encouragement" in db.keys():
-    for quotes in db["encouragement"]:
-      options.append(quotes)
-  
-  #checking if any sad words are in the msg
-  if any(word in msgCont for word in sad_words):
-    await msg.channel.send(random.choice(options))
+  #works only if the bot is set to respond to sad words
+  if db["responding"]:
+    #when the bot chooses the enc quote to display, we also need choice of quotes to be from the user submitted quotes
+    options = start_enc
+    if "encouragement" in db.keys():
+      for quotes in db["encouragement"]:
+        options.append(quotes)
+    #checking if any sad words are in the msg
+    if any(word in msgCont for word in sad_words):
+      await msg.channel.send(random.choice(options))
 
   #the command to add a new encouraging message
   if msgCont.startswith('$new'):
@@ -81,18 +87,46 @@ async def on_message(msg):
   #the command to delete an encouraging message
   if msgCont.startswith('$delete'):
     enc = []
+    
     if "encouragement" in db.keys():
       #because we are converting the number into integer, the space would be gone, therefore no "$delete "
       index = int(msgCont.split("$delete ", 1)[1])
+      
       try:
-        enc = db["encouragement"][index]
-        await msg.channel.send("The quote '" + enc + "' has been deleted." )
+        
+        await msg.channel.send("The quote '" + db["encouragement"][index] + "' has been deleted." )
         del_enc(index)
-        enc = db["encouragement"]
+
       except IndexError as e:
         await msg.channel.send(e)
+    
     else:
       await msg.channel.send("No encouraging messages to delete.")
 
+  #the command to list all the encouraging messages
+  if msgCont.startswith('$list'):
+    enc = []
+    
+    if "encouragement" in db.keys():
+      enc = db["encouragement"]
+    
+    if len(enc) == 0:
+      await msg.channel.send("No encouraging messages to list.")
+    else:
+      for quotes in enc:
+        await msg.channel.send(quotes)
+
+  #the command to turn the bot on or off
+  if msgCont.startswith('$Set responding'):      
+    value = msgCont.split("$Set responding ", 1)[1]
+
+    if value.lower() == "true":
+      db["responding"] = True
+      await msg.channel.send("Responding is on.")
+    else:
+      db["responding"] = False
+      await msg.channel.send("Responding is off.")
+    
 #running the bot
 client.run(os.getenv('BOT1_TOKEN'))
+
